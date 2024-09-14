51b61fdeadb73a9c48952dd2818e25ca56cc6202
Pranav Ahire <1234pranavgo@gmail.com>
2024-09-15 02:10:00 +0530

    Chnaged Home page a bit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import '../globalStyles.css'; // Ensure this import is at the top
 
-// Import the video file
-import backgroundVideo from '../assets/home-bg.mp4';
+// Import the new background video file
+import backgroundVideo from '../assets/clouds-bg.mp4'; // Replace with your new video file path
+
+// Import the GIF file
+import backgroundGif from '../assets/home-bg.gif';
 
 function Home() {
   return (
@@ -12,9 +15,10 @@ function Home() {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <img className="background-gif" src={backgroundGif} alt="Background GIF" />
       <div className="content">
-        <h1>Yo, I am Pranav</h1>
-        <p className="typing-animation">Exploring the World of Code</p>
+        <h1></h1>
+        <p className="typing-animation"> </p>
       </div>
     </div>
   );
